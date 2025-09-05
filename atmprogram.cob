24f6f284932dcb246fbb43dab4b20f35d5769aa3
Diego <dl491945@gmail.com>
2025-09-05 09:27:28 -0600

    file updated

--- atmprogram.cob
+++ atmprogram.cob
@@ -2,30 +2,31 @@
        PROGRAM-ID. bank_account.  
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 Curr PIC $,$$9.99.
-       01 SCurr PIC $,$$9.99.
-       01 OAApproved PIC 9(4)V99 VALUE 1000.
-       01 AmountWithdraw PIC 999V99 VALUE 0.
-       01 AvailableAmount PIC 9(3)V99.
-       01 CurrentAmount PIC 999V99.
+       01 IDNum PIC 9(5).
+       01 Customer.
+           02 OriginalAmount PIC 9(4) VALUE 1000.
+           02 FName PIC X(15).
+           02 LName PIC X(15).
+           02 Withdraw PIC 9(3).
+           02 Balance PIC 9(3)V99.
+      
        01 Dec PIC X(20).
+
        PROCEDURE DIVISION.
        MOVE ALL "*" TO Dec.
        DISPLAY Dec" WELCOME "Dec.
-       DISPLAY "Amount to withdraw: "WITH NO ADVANCING
-       ACCEPT AmountWithdraw
-       IF AmountWithdraw less than 101
-           COMPUTE AvailableAmount = OAApproved - 
-           AmountWithdraw
-           MOVE AmountWithdraw to Curr
-           MOVE AvailableAmount TO CurrentAmount
-           MOVE CurrentAmount TO SCurr
-           DISPLAY "Amount withdrawn: "Curr
-           DISPLAY "Money available: "SCurr
+       DISPLAY "Amount to withdraw: $"WITH NO ADVANCING
+       ACCEPT Withdraw
+       IF Withdraw less than 101
+           COMPUTE Balance = OriginalAmount - Withdraw
+           MOVE Balance TO OriginalAmount
+           DISPLAY "Money available: $"Balance
+           
        ELSE 
            DISPLAY "ERROR: max to withdraw is 100"
        END-IF.
-       
+           
+
        STOP RUN.
        END PROGRAM bank_account.
        
