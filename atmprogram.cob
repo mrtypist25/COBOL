6c956d27ae7105ad60f9c834b2b1784bd8400fd0
Diego <dl491945@gmail.com>
2025-09-05 15:12:48 -0600

    file udated

--- atmprogram.cob
+++ atmprogram.cob
@@ -1,36 +1,55 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. bank_account.  
+       PROGRAM-ID. atm_program.
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+       SELECT DFile ASSIGN TO "customers.txt"
+           ORGANIZATION IS LINE SEQUENTIAL 
+           ACCESS IS SEQUENTIAL.
        DATA DIVISION.
-       WORKING-STORAGE SECTION.
-       01 IDNum PIC 9(5).
+       FILE SECTION.
+       FD DFile.
        01 Customer.
-           02 OriginalAmount PIC 9(4) VALUE 1000.
-           02 FName PIC X(15).
-           02 LName PIC X(15).
+           02 IDnum PIC 9(5).
+           02 spc PIC X(1).
+           02 Loan PIC 9(4).
+           02 CustName.
+               03 FName PIC X(15).
+               03 LName PIC X(15).
+           
+           02 Debt PIC 9(3).
            02 Withdraw PIC 9(3).
            02 Balance PIC 9(3).
-           02 NewAmount PIC 9(3).
-      
-       01 Dec PIC X(20).
-
-       PROCEDURE DIVISION.
-       MOVE ALL "*" TO Dec.
-       DISPLAY Dec" WELCOME "Dec.
-       DISPLAY "Amount to withdraw: $"WITH NO ADVANCING
-       ACCEPT Withdraw
-       IF Withdraw less than 101
-           COMPUTE Balance = OriginalAmount - Withdraw
-           MOVE Balance TO NewAmount
-           DISPLAY "Original amount: $"OriginalAmount
-           DISPLAY "Withdraw: $"Balance
-           DISPLAY "Available amount: $"NewAmount
+       WORKING-STORAGE SECTION.
+       01 WCustomer.
+           02 WLoan PIC 9(4).
+           02 WIDnum PIC 9(5).
+           02 WCustName.
+               03 WFName PIC X(15).
+               03 WLName PIC X(15).
            
-           
-       ELSE 
-           DISPLAY "ERROR: max to withdraw is 100"
-       END-IF.
-           
-
-       STOP RUN.
-       END PROGRAM bank_account.
+           02 WDebt PIC 9(3).
+           02 WWithdraw PIC 9(3).
+           02 WBalance PIC 9(3).
+       01 Dec PIC X(25).
        
+       PROCEDURE DIVISION.
+           MOVE ALL "*" TO Dec.
+               DISPLAY Dec" WELCOME "Dec.
+               DISPLAY "Amount to withdraw: "WITH NO ADVANCING 
+               ACCEPT WWithdraw.
+               COMPUTE WBalance = WLoan - WWithdraw
+               DISPLAY "Actual balance: "WBalance. 
+
+           OPEN OUTPUT DFile
+               MOVE 00001 TO IDnum.
+               MOVE " " TO spc.
+               MOVE "Mike" TO FName.
+               MOVE "White" TO LName.
+               MOVE 1000 TO Loan.
+               MOVE WBalance TO Debt.
+               WRITE Customer
+               END-WRITE.
+           CLOSE DFile.
+       STOP RUN.
+       END PROGRAM atm_program.
