60db764e4891e0e402ffb439fa9b7358245beaad
Diego <dl491945@gmail.com>
2025-09-06 07:18:52 -0600

    code updated

--- atmprogram.cob
+++ atmprogram.cob
@@ -10,13 +10,13 @@
        FILE SECTION.
        FD DFile.
        01 Customer.
-           02 IDnum PIC 9(5).
-           02 spc PIC X(1).
-           02 Loan PIC 9(4).
+           02 IDnum PIC 9(4).
+           
            02 CustName.
                03 FName PIC X(15).
                03 LName PIC X(15).
-           
+           02 Loan PIC 9(4).
+           02 Spc PIC X(1).
            02 Debt PIC 9(3).
            02 Withdraw PIC 9(3).
            02 Balance PIC 9(3).
@@ -24,11 +24,7 @@
        WORKING-STORAGE SECTION.
        01 WCustomer.
            02 WLoan PIC 9(4).
-           02 WIDnum PIC 9(5).
-           02 WCustName.
-               03 WFName PIC X(15).
-               03 WLName PIC X(15).
-           
+           02 WSpc PIC X(1).
            02 WDebt PIC 9(3).
            02 WWithdraw PIC 9(3).
            02 WBalance PIC 9(3).
@@ -46,17 +42,17 @@
                ELSE
                    DISPLAY "The max to withdraw is 300"
                END-IF.
-
-
            OPEN OUTPUT DFile
                MOVE 00001 TO IDnum.
-               MOVE " " TO spc.
                MOVE "Mike" TO FName.
-               MOVE "White" TO LName.
+               MOVE "White" to LName.
                MOVE 1000 TO Loan.
-               MOVE WABalance TO Debt.
+               MOVE " " TO Spc.
+               MOVE WBalance TO Debt.
                WRITE Customer
                END-WRITE.
            CLOSE DFile.
+
+      
        STOP RUN.
        END PROGRAM atm_program.
