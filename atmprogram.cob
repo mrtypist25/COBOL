856044536a388e1e4db188fbc6523fa672bd062b
Diego <dl491945@gmail.com>
2025-09-06 15:35:32 -0600

    file updated

--- atmprogram.cob
+++ atmprogram.cob
@@ -1,58 +1,52 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. atm_program.
        ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
        INPUT-OUTPUT SECTION.
-       FILE-CONTROL.
-       SELECT DFile ASSIGN TO "customers.txt"
+       FILE-CONTROL. 
+       SELECT DataFile ASSIGN TO "customers.txt"
            ORGANIZATION IS LINE SEQUENTIAL 
-           ACCESS IS SEQUENTIAL.
+           ACCESS IS SEQUENTIAL. 
        DATA DIVISION.
-       FILE SECTION.
-       FD DFile.
-       01 Customer.
-           02 IDnum PIC 9(4).
-           
-           02 CustName.
-               03 FName PIC X(15).
-               03 LName PIC X(15).
-           02 Loan PIC 9(4).
-           02 Spc PIC X(1).
-           02 Debt PIC 9(3).
-           02 Withdraw PIC 9(3).
-           02 Balance PIC 9(3).
-           02 ABalance PIC 9(3).
+       FILE SECTION. 
+       FD DataFile.
+       01 Table1.
+           02 DData PIC X(15) OCCURS 6 TIMES.
        WORKING-STORAGE SECTION.
-       01 WCustomer.
-           02 WLoan PIC 9(4).
-           02 WSpc PIC X(1).
-           02 WDebt PIC 9(3).
-           02 WWithdraw PIC 9(3).
-           02 WBalance PIC 9(3).
-           02 WABalance PIC 9(3).
-       01 Dec PIC X(25).
-       
+       01 WIDnum PIC 9(5) VALUE 00002.
+       01 WFname PIC X(15) VALUE "Michael".
+       01 WLname PIC X(15) VALUE "White".
+       01 WLoan  PIC 9(4) VALUE 1000.
+       01 WDebt PIC 9(3) VALUE 000.
+       01 WWithdraw PIC 9(3).
+       01 WBalance PIC 9(3).
+       01 WDec PIC X(25).
        PROCEDURE DIVISION.
-           MOVE ALL "*" TO Dec.
-               DISPLAY Dec" WELCOME "Dec.
-               DISPLAY "Amount to withdraw: "WITH NO ADVANCING 
-               ACCEPT WWithdraw.
-               IF WWithdraw < 301
-                   COMPUTE WBalance = WLoan - WWithdraw
-                   DISPLAY "Actual balance: "WBalance
-               ELSE
-                   DISPLAY "The max to withdraw is 300"
-               END-IF.
-           OPEN OUTPUT DFile
-               MOVE 00001 TO IDnum.
-               MOVE "Mike" TO FName.
-               MOVE "White" to LName.
-               MOVE 1000 TO Loan.
-               MOVE " " TO Spc.
-               MOVE WBalance TO Debt.
-               WRITE Customer
-               END-WRITE.
-           CLOSE DFile.
-
+       MOVE ALL "*" TO WDec.
+       DISPLAY WDec" WELCOME "WDec.
+       DISPLAY "Amount to withdraw: $"WITH NO ADVANCING
+       ACCEPT WWithdraw.
+       IF WWithdraw < 301
+           COMPUTE WBalance = WDebt + WWithdraw
+           MOVE WBalance TO WDebt
+           DISPLAY "You withdrawn $: "WWithdraw
+           COMPUTE WBalance = WLoan - WWithdraw
+           MOVE WBalance TO WLoan
       
+       ELSE 
+           DISPLAY "Max to withdraw is $300.00"
+       END-IF.
+       OPEN OUTPUT DataFile
+           MOVE WIDnum TO DData(1).
+           MOVE WFname TO DData(2).
+           MOVE WLname TO DData(3).
+           MOVE WLoan TO DData(4).
+           MOVE WDebt TO DData(5)
+           WRITE Table1
+           END-WRITE.
+
+       CLOSE DataFile.        
+           
+           
+           
        STOP RUN.
-       END PROGRAM atm_program.
