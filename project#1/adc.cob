0e014b836c526cf7e74ca2f62b85fa19f90d3d3d
Diego <dl491945@gmail.com>
2025-09-25 16:36:04 -0600

    file updated

--- project#1/adc.cob
+++ project#1/adc.cob
@@ -108,8 +108,9 @@
                ACCEPT sName
            
            DISPLAY"City name: "WITH NO ADVANCING 
-               ACCEPT cName
-           DISPLAY"Type any key and hit enter to finish: "WITH NO ADVANCING
+           ACCEPT cName
+           DISPLAY"Type any key and hit enter to finish: "
+           WITH NO ADVANCING
            ACCEPT spc
            MOVE SPACE TO spc
 
